442f96f5a4d39e8dc07a1c3c247dd43617543161
ichetanpawar <66332051+ichetanpawar@users.noreply.github.com>
2021-04-18 02:15:57 +0530

    Update Accounts.java

--- src/test/java/pageObjects/Accounts.java
+++ src/test/java/pageObjects/Accounts.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Accounts extends Common {
 
